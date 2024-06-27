a632f7e1e24dd28bf7b85d338c9f4e22cb34b71d
SuiyueYoung <yyh1909824684@gmail.com>
2024-06-27 13:30:24 +0800

    for test

---
+++ src/main/java/com/rectangle/onlinehospital/pojo/User.java
@@ -0,0 +1,8 @@
+package com.rectangle.onlinehospital.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer userID;
+}
---
+++ src/test/java/com/rectangle/onlinehospital/OnlineHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rectangle.onlinehospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineHospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
