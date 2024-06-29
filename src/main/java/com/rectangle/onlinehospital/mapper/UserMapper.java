8b7d2b248ef10903318588cb43e40149ff94091e
SuiyueYoung <yyh1909824684@gmail.com>
2024-06-29 13:05:01 +0800

    update

--- src/main/java/com/rectangle/onlinehospital/mapper/UserMapper.java
+++ src/main/java/com/rectangle/onlinehospital/mapper/UserMapper.java
@@ -3,15 +3,8 @@ package com.rectangle.onlinehospital.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.rectangle.onlinehospital.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
-    @Select("SELECT * FROM users WHERE userId=#{userID}")
-    User findByUserID(String userID);
 
-    @Select("SELECT * FROM users")
-    List<User> findAllUser();
 }
