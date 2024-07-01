0f221d165011b69728140b3bfb84ff660acaad6e
SuiyueYoung <yyh1909824684@gmail.com>
2024-07-01 23:49:02 +0800

    update

--- src/main/java/com/rectangle/onlinehospital/controller/HospitalController.java
+++ src/main/java/com/rectangle/onlinehospital/controller/HospitalController.java
@@ -5,11 +5,13 @@ import com.rectangle.onlinehospital.service.HospitalService;
 import com.rectangle.onlinehospital.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/hospital")
+@RestController
+@RequestMapping("/hospital")
 public class HospitalController {
 
     private final HospitalService hospitalService;
--- src/main/java/com/rectangle/onlinehospital/controller/SetMealController.java
+++ src/main/java/com/rectangle/onlinehospital/controller/SetMealController.java
@@ -1,8 +1,10 @@
 package com.rectangle.onlinehospital.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/setMeal")
+@RestController
+@RequestMapping("/setMeal")
 public class SetMealController {
 
 }
