fcccb88e3e36b80728b3a819ffecf9c5bc9f7cd7
X1a0hu <2816050593@qq.com>
2024-07-02 10:42:57 +0800

    2024/7/2

--- src/main/java/com/rectangle/onlinehospital/controller/SetMealController.java
+++ src/main/java/com/rectangle/onlinehospital/controller/SetMealController.java
@@ -8,6 +8,7 @@ import com.rectangle.onlinehospital.service.CheckItemService;
 import com.rectangle.onlinehospital.service.SetMealDetailedService;
 import com.rectangle.onlinehospital.service.SetMealService;
 import com.rectangle.onlinehospital.utils.Result;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,7 +27,14 @@ public class SetMealController {
 
     private final CheckItemService checkItemService;
 
-
+    /**
+     * @Author X1a0hu
+     * @Date 2024/7/2 上午10:02
+     * @Description
+     * @Param [setMealService, setMealDetailedService, checkItemService]
+     * @Return
+     * @Since version 1.0
+     */
     @Autowired
     public SetMealController(SetMealService setMealService, SetMealDetailedService setMealDetailedService, CheckItemService checkItemService) {
         this.setMealService = setMealService;
@@ -34,6 +42,14 @@ public class SetMealController {
         this.checkItemService = checkItemService;
     }
 
+    /**
+     * @Author X1a0hu
+     * @Date 2024/7/2 上午10:01
+     * @Description
+     * @Param
+     * @Return
+     * @Since version 1.0
+     */
     @GetMapping("/getAll")
     public Result<List<SetMeal>> list() {
         List<SetMeal> setmealList = setMealService.list();
